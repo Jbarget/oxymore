faadba94fa1a0657c38eb436312fb532ab0b31be
Justen Barget <Jbarget@users.noreply.github.com>
2020-06-15 16:07:52 +0200

    adds theme and styled-system to project (#26)
    * adds theme and styled-system to project #20

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
+import theme from "./components/theme";
+
+import "./css/reset.css";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          {" "}
-          // load only one route at a time
-          <Route path="/" exact component={Home} /> //renders only if it matches
-          the full path
-          <Route path="/projects" exact component={Projects} />
-        </Switch>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/projects" exact component={Projects} />
+          </Switch>
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import styled from "styled-components";
+import {
+  color,
+  space,
+  typography,
+  TypographyProps,
+  ColorProps,
+  SpaceProps,
+} from "styled-system";
+
+type HeadingProps = ColorProps & SpaceProps & TypographyProps;
+
+const Title = styled.h1<HeadingProps>`
+  ${color}
+  ${space}
+  ${typography}
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Title color="primary" bg="error" p={3} fontSize={3}>
+      Oxymore
+    </Title>
+  );
 };
 
 export default Home;
