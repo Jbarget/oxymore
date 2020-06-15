bcf4cba3a0839641e6b76f94d5caa66703e81e70
sophie <sophieleila@icloud.com>
2020-06-15 15:46:48 +0200

    PR comments

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,5 @@
 import React, { Component } from "react";
 
-// class Home extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   render() {
-//     return <div></div>;
-//   }
-// }
-
 const Home = () => {
   return <div>Home</div>;
 };
