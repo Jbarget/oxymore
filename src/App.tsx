2618411e0e85cf83c527b07a9cb15c13855e166d
sophie <sophieleila@icloud.com>
2020-06-15 13:40:19 +0200

    set up react router with basic components #24

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          {" "}
+          // load only one route at a time
+          <Route path="/" exact component={Home} /> //renders only if it matches
+          the full path
+          <Route path="/projects" exact component={Projects} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
