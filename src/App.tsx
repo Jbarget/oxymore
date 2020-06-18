bc0e96612cf13bf5dce78460d1e7dd36de99727b
sophie <sophieleila@icloud.com>
2020-06-18 12:46:28 +0200

    reposition nav button inside header #3

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import theme from "./components/theme";
-import NavMenu from "./components/NavMenu";
-
 import "./css/reset.css";
 
 const App = () => {
@@ -14,7 +12,6 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <div className="App">
           <Suspense fallback={<div>Loading</div>}>
-            <NavMenu />
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/projects" exact component={Projects} />
