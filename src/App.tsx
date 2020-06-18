926fb6756d35993cb1ee44a431aa73891cf1395c
sophie <sophieleila@icloud.com>
2020-06-18 11:37:35 +0200

    add burger menu functionality #3

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import theme from "./components/theme";
+import NavMenu from "./components/NavMenu";
 
 import "./css/reset.css";
 
@@ -13,6 +14,7 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <div className="App">
           <Suspense fallback={<div>Loading</div>}>
+            <NavMenu />
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/projects" exact component={Projects} />
---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,44 @@
+import React, { useState, Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import styled, { css } from "styled-components";
+import {
+  color,
+  space,
+  typography,
+  TypographyProps,
+  ColorProps,
+  SpaceProps,
+} from "styled-system";
+
+type HeadingProps = ColorProps & SpaceProps & TypographyProps;
+
+const MenuButton = styled.button<HeadingProps>`
+${color}
+${space}
+${typography}`;
+
+const visibleStyles = css`
+  display: flex;
+`;
+const NavMenuOverlay = styled.dialog<{ expanded: boolean }>`
+  display: none;
+  ${(props) => props.expanded && visibleStyles}
+`;
+
+const NavMenu = () => {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <Fragment>
+      <MenuButton onClick={() => setExpanded(!expanded)}>click me</MenuButton>
+      <NavMenuOverlay expanded={expanded}>
+        <NavLink to="/manifesto">MANIFESTO</NavLink>
+        <NavLink to="/advertising">ADVERTISING</NavLink>
+        <NavLink to="/about-us">ABOUT US</NavLink>
+        <NavLink to="/contact">CONTACT</NavLink>
+        <NavLink to="/buy">BUY</NavLink>
+      </NavMenuOverlay>
+    </Fragment>
+  );
+};
+
+export default NavMenu;
