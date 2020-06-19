d6aafc97d85f5bf193331749110996249a7265b2
sophie <sophieleila@icloud.com>
2020-06-19 12:31:29 +0200

    apply styles to nav menu text

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,15 +3,6 @@ import { NavLink } from "react-router-dom";
 import styled, { css } from "styled-components";
 import { useTranslation } from "react-i18next";
 
-import {
-  color,
-  space,
-  typography,
-  TypographyProps,
-  ColorProps,
-  SpaceProps,
-} from "styled-system";
-
 const visibleStyles = css`
   display: flex;
   flex-direction: column;
@@ -21,6 +12,7 @@ const visibleStyles = css`
   justify-content: center;
   text-align: center;
   width: 100%;
+  font-family: SangBleu OG Serif Light Regular;
 `;
 const NavMenuOverlay = styled.dialog<{ isOpen: boolean }>`
   display: none;
@@ -31,22 +23,48 @@ const MenuButton = styled.button`
   position: absolute;
   right: 1em;
   top: 1em;
+  height: 50px;
+  width: 50px;
+  background-color: #555;
+`;
+
+const MenuItem = styled.h1`
+  color: #000000;
+  padding: 0.5em;
+  &:focus,
+  &:hover {
+    font-size: 40px;
+  }
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
 `;
 
 const NavMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const { t } = useTranslation();
 
   return (
     <Fragment>
       <MenuButton onClick={() => setIsOpen(!isOpen)}>click me</MenuButton>
       <NavMenuOverlay isOpen={isOpen} onClick={() => setIsOpen(false)}>
-        <NavLink to="/manifesto">{t("nav.navLink1")} </NavLink>
-        <NavLink to="/advertising">{t("nav.navLink2")}</NavLink>
-        <NavLink to="/about-us">{t("nav.navLink3")}</NavLink>
-        <NavLink to="/contact">{t("nav.navLink4")}</NavLink>
-        <NavLink to="/buy">{t("nav.navLink5")}</NavLink>
+        <NavLink to="/manifesto" style={{ textDecoration: "none" }}>
+          <MenuItem>{t("nav.manifesto")} </MenuItem>
+        </NavLink>
+        <NavLink to="/advertising" style={{ textDecoration: "none" }}>
+          <MenuItem>{t("nav.advertising")}</MenuItem>
+        </NavLink>
+        <NavLink to="/about-us" style={{ textDecoration: "none" }}>
+          <MenuItem>{t("nav.aboutUs")}</MenuItem>
+        </NavLink>
+        <NavLink to="/contact" style={{ textDecoration: "none" }}>
+          <MenuItem>{t("nav.contact")}</MenuItem>
+        </NavLink>
+        <NavLink to="/buy" style={{ textDecoration: "none" }}>
+          <MenuItem>{t("nav.buy")}</MenuItem>
+        </NavLink>
       </NavMenuOverlay>
     </Fragment>
   );
