89d3c772af724b6b38de4bc51225f333a73dbf44
sophie <sophieleila@icloud.com>
2020-06-24 00:26:51 +0200

    add background image to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback } from "react";
-import styled from "styled-components";
+import React, { useCallback, Fragment } from "react";
+import styled, { css } from "styled-components";
 import { space, SpaceProps } from "styled-system";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
+import Background from "./assets/fondo-web.jpg";
 
 type HeadingProps = SpaceProps;
 
@@ -23,7 +24,14 @@ const Home = () => {
     []
   );
   return (
-    <div>
+    <div
+      className="bg_image"
+      style={{
+        backgroundImage: "url(" + Background + ")",
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
       <Title p={3}>OXYMORE</Title>
       <LangButton onClick={onLangClick("en")}>EN</LangButton>
       <LangButton onClick={onLangClick("es")}>ES</LangButton>
