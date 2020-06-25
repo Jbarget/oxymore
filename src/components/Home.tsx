71df726ba341b756911ef3ac1b28f8be1a716493
sophie <sophieleila@icloud.com>
2020-06-25 15:59:18 +0200

    move language buttons to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Background from "./assets/home-page/background.jpg";
 import {
   color,
   space,
@@ -13,13 +15,39 @@ import i18next from "i18next";
 
 type HeadingProps = ColorProps & SpaceProps & TypographyProps;
 
+const Header = styled.div<HeadingProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
 const Title = styled.h1<HeadingProps>`
+  ${space}
+  ${color}
+  ${typography}
+  font-family: SangBleu OG Serif Light Regular;
+`;
+
+const LangButton = styled.button<HeadingProps>`
   ${color}
   ${space}
   ${typography}
 `;
 
-const LangButton = styled.button``;
+const Container = styled.div`
+  font-family: SangBleu OG Serif Light Regular;
+  height: auto;
+  position: absolute;
+  float: right;
+  right: 100px;
+  display: flex;
+  cursor: pointer;
+  p:hover,
+  p:active {
+    color: black;
+  }
+`;
 
 const Home = () => {
   const { t } = useTranslation();
@@ -29,13 +57,41 @@ const Home = () => {
   );
 
   return (
-    <main>
-      <Title color="primary" bg="error" p={3} fontSize={3}>
-        {t("home.header")}
-      </Title>
-      <LangButton onClick={onLangClick("en")}>EN</LangButton>
-      <LangButton onClick={onLangClick("es")}>ES</LangButton>
-    </main>
+    <div
+      className="bg_image"
+      style={{
+        backgroundImage: "url(" + Background + ")",
+        backgroundSize: "cover",
+        height: "140vh",
+      }}
+    >
+      {" "}
+      <Header>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Title p={7} color="athensGray" fontSize={5}>
+            OXYMORE
+          </Title>
+        </Link>
+      </Header>
+      <Container>
+        <LangButton
+          onClick={onLangClick("en")}
+          color="athensGray"
+          fontSize={3}
+          p={1}
+        >
+          EN
+        </LangButton>
+        <LangButton
+          onClick={onLangClick("es")}
+          color="athensGray"
+          fontSize={3}
+          p={1}
+        >
+          ES
+        </LangButton>
+      </Container>
+    </div>
   );
 };
 
