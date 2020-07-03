b4f6d06daf82ac0d289f1a4cdd6ed955e7c58e18
sophie <sophieleila@icloud.com>
2020-07-03 19:49:33 +0200

    create projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,26 @@
-import React, { Fragment } from "react";
+import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import {
-  color,
   space,
-  typography,
-  TypographyProps,
-  ColorProps,
   SpaceProps,
+  layout,
+  LayoutProps,
+  background,
+  BackgroundProps,
+  FlexboxProps,
+  flexbox,
+  PositionProps,
+  position,
+  ColorProps,
+  color,
+  TypographyProps,
+  typography,
+  border,
+  BorderProps,
 } from "styled-system";
 import stairs from "./assets/project-page/stairs.png";
-import shell from "./assets/project-page/knife.png";
+import shell from "./assets/project-page/shell.png";
 import eye from "./assets/project-page/eye.png";
 import statue from "./assets/project-page/statue.png";
 import dragon from "./assets/project-page/dragon.png";
@@ -18,99 +29,194 @@ import mask from "./assets/project-page/mask.png";
 import spider from "./assets/project-page/spider.png";
 import magnify from "./assets/project-page/magnify.png";
 
-const IconContainer = styled.div`
-  width: 200px;
-  height: 120px;
+type BgProps = BackgroundProps &
+  ColorProps &
+  LayoutProps &
+  FlexboxProps &
+  TypographyProps &
+  LayoutProps &
+  SpaceProps;
+
+const Background = styled.div<BgProps>`
+  ${background};
+  ${color};
+  ${layout};
+  ${flexbox};
+  ${space};
 `;
 
-const Icon = styled.a`
+type LogoProps = SpaceProps &
+  LayoutProps &
+  FlexboxProps &
+  PositionProps &
+  ColorProps &
+  SpaceProps &
+  TypographyProps &
+  BorderProps &
+  ColorProps;
+
+const HomeLink = styled.a<LogoProps>`
+  ${space};
+`;
+
+const HomeLogo = styled.p<LogoProps>`
+  ${flexbox};
+  ${typography}
+  ${color}
+`;
+
+const BuyButton = styled.button<LogoProps>`
+  ${space};
+  ${layout};
+  ${color};
+  ${typography};
+  ${flexbox};
+  ${border}
+  ${position};
+`;
+
+const IconsContainer = styled.div<LogoProps>`
+  // ${flexbox};
+  // ${position};
+  // ${space};
+`;
+
+const Icon = styled.a<LogoProps>`
+  ${layout};
+  ${position};
+  ${flexbox};
+  ${space};
+  ${position};
   a:hover {
-    position: relative;
-    width: 500px;
-    height: auto;
+    width: 2em;
   }
 `;
 
 const Projects = () => {
   return (
-    <Fragment>
-      <IconContainer>
-        <Icon>
-          <img
-            src={stairs}
-            style={{
-              position: "relative",
-              top: 100,
-              left: 100,
-            }}
-          ></img>
+    <Background
+      backgroundSize="cover"
+      backgroundImage="url('./assets/home-page/background.jpg')"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      bg="black"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      mx="auto"
+    >
+      <HomeLink p={30}>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <HomeLogo
+            fontFamily="SangBleu OG Serif Light Regular"
+            color="athensGray"
+            fontSize={[null, 16, 18, 28, 28]}
+          >
+            OXYMORE
+          </HomeLogo>
+        </Link>
+      </HomeLink>
+      <IconsContainer>
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[80, 80, 80, 180, 180]}
+          left={[160, 160, 180, 120, 160]}
+        >
+          <img src={stairs}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={shell}
-            style={{ position: "relative", top: 240, left: 100 }}
-          ></img>
+
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[140, 140, 140, 360, 360]}
+          left={[100, 100, 120, 120, 160]}
+        >
+          <img src={shell}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={eye}
-            style={{ position: "relative", bottom: 240, left: 420 }}
-          ></img>
+
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[160, 160, 160, 140, 140]}
+          left={[200, 200, 240, 280, 340]}
+        >
+          <img src={eye}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={statue}
-            style={{ position: "relative", top: 54, left: 420 }}
-          ></img>
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[260, 260, 260, 360, 400]}
+          left={[200, 200, 240, 280, 340]}
+        >
+          <img src={statue}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={dragon}
-            style={{ position: "relative", bottom: 260, left: 720 }}
-          ></img>
+
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[240, 240, 240, 240, 280]}
+          left={[120, 120, 160, 400, 500]}
+        >
+          <img src={dragon}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={knife}
-            style={{ position: "relative", bottom: 40, left: 680 }}
-          ></img>
+
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[400, 400, 400, 440, 500, 500]}
+          left={[100, 100, 120, 380, 500]}
+        >
+          <img src={knife}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={mask}
-            style={{ position: "relative", bottom: 720, left: 1020 }}
-          ></img>
+
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[380, 380, 380, 140, 140]}
+          left={[180, 180, 220, 500, 640]}
+        >
+          <img src={mask}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={spider}
-            style={{ position: "relative", bottom: 320, left: 1020 }}
-          ></img>
+
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[320, 320, 320, 400, 440]}
+          left={[100, 100, 120, 500, 680]}
+        >
+          <img src={spider}></img>
         </Icon>
-      </IconContainer>
-      <IconContainer>
-        <Icon>
-          <img
-            src={magnify}
-            style={{ position: "relative", bottom: 660, left: 1210 }}
-          ></img>
+        <Icon
+          width={[50, 60, 60, 100, 120]}
+          position="absolute"
+          top={[480, 480, 480, 260, 300]}
+          left={[160, 160, 180, 600, 800]}
+        >
+          <img src={magnify}></img>
         </Icon>
-      </IconContainer>
-    </Fragment>
+      </IconsContainer>
+
+      <BuyButton
+        position="absolute"
+        fontFamily="SangBleu OG Serif Light Regular"
+        color="athensGray"
+        p={8}
+        py={1}
+        bg="transparent"
+        bottom={70}
+        border={1}
+        borderColor="athensGray"
+        borderStyle="solid"
+        fontSize={[null, 12, 14, 16, 16]}
+        display="flex"
+        justifyContent="center"
+        right={[null, null, null, 40, 40]}
+      >
+        BUY
+      </BuyButton>
+    </Background>
   );
 };
 
