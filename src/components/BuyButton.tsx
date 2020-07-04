5654f1ae022ca3ac87005a39cd41b30dcb1845a2
sophie <sophieleila@icloud.com>
2020-07-04 12:11:13 +0200

    PR comments #18

--- src/components/BuyButton.tsx
+++ src/components/BuyButton.tsx
@@ -2,36 +2,34 @@ import React, { useState, Fragment } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(
-  process.env.REACT_APP_STRIPE_API_KEY as string
-);
+const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_API_KEY}`);
 
 const BuyButton = () => {
   const [error, setError] = useState<string>();
 
   const handleClick = async () => {
-    // When the customer clicks on the button, redirect them to Checkout.
-    const stripe = await stripePromise;
-    if (!stripe) {
-      return null;
-    }
-    const transaction = await stripe.redirectToCheckout({
-      lineItems: [
-        { price: process.env.REACT_APP_STRIPE_PRICE_ID as string, quantity: 1 },
-      ],
-      mode: "payment",
-      successUrl: `${process.env.REACT_APP_BASE_URL as string}/projects`,
-      cancelUrl: process.env.REACT_APP_BASE_URL as string,
-      shippingAddressCollection: {
-        allowedCountries: ["ES", "FR", "GB"],
-      },
-    });
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `error.message`.
-
-    if (transaction.error) {
-      setError(transaction.error.message);
+    try {
+      // When the customer clicks on the button, redirect them to Checkout.
+      const stripe = await stripePromise;
+      if (!stripe) {
+        return null;
+      }
+      const transaction = await stripe.redirectToCheckout({
+        lineItems: [
+          { price: `${process.env.REACT_APP_STRIPE_PRICE_ID}`, quantity: 1 },
+        ],
+        mode: "payment",
+        successUrl: `${process.env.REACT_APP_BASE_URL}/projects`,
+        cancelUrl: `${process.env.REACT_APP_BASE_URL}`,
+        shippingAddressCollection: {
+          allowedCountries: ["ES", "FR", "GB"],
+        },
+      });
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `error.message`.
+    } catch (error) {
+      setError(error.message);
     }
   };
 
