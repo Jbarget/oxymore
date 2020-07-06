005111d9320230d3380f42dddf55ce87ac80f0b8
sophie <sophieleila@icloud.com>
2020-07-06 16:23:31 +0200

    minor edit to responsive styles

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App = () => {
               <LangButton
                 onClick={onLangClick("en")}
                 color="athensGray"
-                fontSize={3}
+                fontSize={[2, 2, 2, 3, 4]}
                 p={1}
               >
                 EN
@@ -65,7 +65,7 @@ const App = () => {
               <LangButton
                 onClick={onLangClick("es")}
                 color="athensGray"
-                fontSize={3}
+                fontSize={[2, 2, 2, 3, 4]}
                 p={1}
               >
                 ES
