93140bc64ce2711326807c0126677cdb492604a6
sssoja <53219789+sssoja@users.noreply.github.com>
2020-07-07 16:14:43 +0200

    So/new home page (#40)
    * create homepage with responsive styles
    
    * add image assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,76 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useCallback } from "react";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import theme from "./components/theme";
-import NavMenu from "./components/NavMenu";
+import styled from "styled-components";
+import i18next from "i18next";
 import "./css/reset.css";
+import {
+  color,
+  ColorProps,
+  space,
+  SpaceProps,
+  typography,
+  TypographyProps,
+  layout,
+  LayoutProps,
+  position,
+  PositionProps,
+} from "styled-system";
+
+type LanguageButtonProps = SpaceProps &
+  ColorProps &
+  TypographyProps &
+  PositionProps &
+  LayoutProps;
+
+const Container = styled.div<LanguageButtonProps>`
+  ${position}
+  ${layout}
+  cursor: pointer;
+`;
+
+const LangButton = styled.p<LanguageButtonProps>`
+  ${typography}
+  ${color}
+  ${space}
+`;
 
 const App = () => {
+  const onLangClick = useCallback(
+    (countryId: string) => () => i18next.changeLanguage(countryId),
+    []
+  );
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <div className="App">
           <Suspense fallback={<div>Loading</div>}>
-            <NavMenu />
+            <Container
+              position="absolute"
+              right={[60, 60, 60, 90, 90]}
+              top={30}
+              display="flex"
+            >
+              <LangButton
+                onClick={onLangClick("en")}
+                color="athensGray"
+                fontSize={[2, 2, 2, 3, 4]}
+                p={1}
+              >
+                EN
+              </LangButton>
+              <LangButton
+                onClick={onLangClick("es")}
+                color="athensGray"
+                fontSize={[2, 2, 2, 3, 4]}
+                p={1}
+              >
+                ES
+              </LangButton>
+            </Container>
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/projects" exact component={Projects} />
