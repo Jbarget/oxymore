b397e0d652449bbc6ccc1a5b0f0cd741ba57adc0
sophie <sophieleila@icloud.com>
2020-07-18 17:46:18 +0200

    so/contact page 2.0

---
+++ src/components/Contact.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+import {
+  color,
+  space,
+  typography,
+  layout,
+  flexbox,
+  ColorProps,
+  SpaceProps,
+  TypographyProps,
+  LayoutProps,
+  FlexboxProps,
+} from "styled-system";
+
+const Main = styled.main<
+  LayoutProps & FlexboxProps & SpaceProps & TypographyProps
+>`
+  background-color: black;
+  display: flex;
+  min-height: 100vh;
+  ${layout};
+  ${flexbox};
+  ${space};
+  ${typography};
+`;
+
+const H1 = styled.h1<ColorProps & SpaceProps & FlexboxProps & TypographyProps>`
+  text-transform: uppercase;
+  ${color};
+  ${space};
+  ${flexbox};
+  ${typography};
+`;
+
+const H2 = styled.h2<ColorProps & SpaceProps & FlexboxProps & TypographyProps>`
+  text-transform: uppercase;
+  ${color};
+  ${space};
+  ${flexbox};
+  ${typography};
+`;
+
+const ContactInfo = styled.p<SpaceProps & FlexboxProps & TypographyProps>`
+  text-transform: uppercase;
+  color: #f4f4f6;
+  ${space};
+  ${flexbox};
+  ${typography};
+`;
+
+const Contact = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Main
+      p={6}
+      display="flex"
+      alignItems="center"
+      textAlign={["center", "start"]}
+    >
+      <ul>
+        <H1 fontSize={[4, 5, 6, 7]} py={3}>
+          {t("contact.header")}
+        </H1>
+        <H2 fontSize={[2, 3, 4, 5]} py={3}>
+          {t("contact.subheader")}
+        </H2>
+        <ContactInfo fontSize={[2, 3, 4, 5]} py={1}>
+          <a href="mailto:ox@oxymore.com" target="_blank">
+            {t("contact.email")}
+          </a>
+        </ContactInfo>
+        <ContactInfo fontSize={[2, 3, 4, 5]}>
+          {t("contact.location")}
+        </ContactInfo>
+      </ul>
+    </Main>
+  );
+};
+
+export default Contact;
