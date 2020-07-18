4d5c298736a4daf3204c1612893732f32968d6a8
sssoja <53219789+sssoja@users.noreply.github.com>
2020-07-18 17:43:45 +0200

    so/contact page (#49)
    * so/contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import theme from "./components/theme";
 import styled from "styled-components";
 import i18next from "i18next";
@@ -78,6 +79,7 @@ const App = () => {
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/projects" exact component={Projects} />
+              <Route path="/contact-us" exact component={Contact} />
             </Switch>
           </Suspense>
         </div>
