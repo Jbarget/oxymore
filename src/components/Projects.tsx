218502afcc957eb19af4fae399738cdade690ecf
sophie <sophieleila@icloud.com>
2020-09-14 17:11:31 +0200

    moves buy button to bottom of screen on desktop #129

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -45,8 +45,9 @@ const Container = styled.div<
   overflow: hidden;
 `;
 
-const BuyButtonContainer = styled.div<PositionProps>`
+const BuyButtonContainer = styled.div<PositionProps & SpaceProps>`
   ${position};
+  ${space};
 `;
 const Img = styled.img<LayoutProps & GridProps & FlexboxProps>`
   ${layout};
@@ -142,7 +143,12 @@ const Projects = () => {
           justifySelf={["center", "center"]}
         />
       </Container>
-      <BuyButtonContainer position="fixed" top="50%" zIndex={zIndexes.inFront}>
+      <BuyButtonContainer
+        position="fixed"
+        bottom={["40%", "40%", "0%", "0%"]}
+        p={6}
+        zIndex={zIndexes.inFront}
+      >
         <BuyButton
           successUrl={`${process.env.REACT_APP_BASE_URL}/projects`}
           cancelUrl={`${process.env.REACT_APP_BASE_URL}/projects`}
