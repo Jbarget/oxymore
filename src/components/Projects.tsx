2bc55633d4cf3b775e2fb6a20503afc784e1e0b9
sophie <sophieleila@icloud.com>
2020-09-15 20:13:34 +0200

    polishes project icons with responsive styles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -45,13 +45,17 @@ const Container = styled.div<
   overflow: hidden;
 `;
 
-const BuyButtonContainer = styled.div<PositionProps>`
+const BuyButtonContainer = styled.div<PositionProps & SpaceProps>`
   ${position};
+  ${space};
 `;
 const Img = styled.img<LayoutProps & GridProps & FlexboxProps>`
   ${layout};
   ${grid};
   ${flexbox};
+  transition: transform 0.2s;
+  &:hover {
+    transform: scale(1.05);
 `;
 type ScrollbackProps = PositionProps &
   TypographyProps &
@@ -81,68 +85,72 @@ const Projects = () => {
     >
       <Container
         gridTemplateColumns="repeat(3, 1fr)"
+        gridTemplateRows="repeat(4, 1fr)"
         display="grid"
-        height="100%"
+        justifyItems="center"
+        alignItems="center"
       >
         <Img
           src={stairs}
-          gridColumn={[2, 2, 1]}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center", "end"]}
+          gridColumn={[2, 2, 2, 2, 3]}
+          gridRow={[1, 1, 1, 1, "1/3"]}
+          maxWidth={["100%", "100%", "100%", "100%", "30%"]}
         />
         <Img
           src={shell}
-          gridColumn={2}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center"]}
-          alignSelf={["initial", "initial", "center"]}
+          gridColumn={[2, 2, 2, 2, "2/4"]}
+          gridRow={[2, 2, 2, 2, 1]}
+          maxWidth={["100%", "100%", "100%", "100%", "15%"]}
         />
         <Img
           src={eye}
-          gridColumn={[2, 2, 3]}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center"]}
+          gridColumn={[2, 2, 2, 2, 1]}
+          gridRow={[3, 3, 3, 3, 1]}
+          maxWidth={["100%", "100%", "100%", "100%", "30%"]}
         />
         <Img
           src={statue}
-          gridColumn={[2, 2, 1]}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center"]}
+          gridColumn={[2, 2, 2, 2, 1]}
+          gridRow={[4, 4, 4, 4, "2/4"]}
+          maxWidth={["100%", "100%", "100%", "100%", "30%"]}
         />
         <Img
           src={dragon}
-          gridColumn={2}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center", "start"]}
-          alignSelf={["initial", "initial", "end"]}
+          gridColumn={[2, 2, 2, 2]}
+          gridRow={[5, 5, 5, 5, 2]}
+          maxWidth={["100%", "100%", "100%", "100%", "30%"]}
         />
         <Img
           src={knife}
-          gridColumn={[2, 2, 3]}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center", "start"]}
+          gridColumn={[2, 2, 2, 2, 3]}
+          gridRow={[6, 6, 6, 6, 3]}
+          maxWidth={["100%", "100%", "100%", "100%", "30%"]}
         />
         <Img
           src={mask}
-          gridColumn={[2, 2, 1]}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center"]}
+          gridColumn={[2, 2, 2, 2, "1/3"]}
+          gridRow={[7, 7, 7, 7, "1/3"]}
+          maxWidth={["100%", "100%", "100%", "100%", "15%"]}
         />
         <Img
           src={spider}
-          gridColumn={[2, 2, 2]}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center"]}
-          alignSelf={["initial", "initial", "center"]}
+          gridColumn={[2, 2, 2, 2, "1/3"]}
+          gridRow={[8, 8, 8, 8, 3]}
+          maxWidth={["100%", "100%", "100%", "100%", "15%"]}
         />
         <Img
           src={magnify}
-          gridColumn={[2, 2, 3]}
-          maxWidth={["75%", "75%", "50%", "30%"]}
-          justifySelf={["center", "center"]}
+          gridColumn={[2, 2, 2, 2, "2/4"]}
+          gridRow={[9, 9, 9, 9, 3]}
+          maxWidth={["100%", "100%", "100%", "100%", "15%"]}
         />
       </Container>
-      <BuyButtonContainer position="fixed" top="50%" zIndex={zIndexes.inFront}>
+      <BuyButtonContainer
+        position="fixed"
+        bottom={["40%", "40%", "40%", "40%", "5%"]}
+        p={6}
+        zIndex={zIndexes.inFront}
+      >
         <BuyButton
           successUrl={`${process.env.REACT_APP_BASE_URL}/projects`}
           cancelUrl={`${process.env.REACT_APP_BASE_URL}/projects`}
@@ -150,7 +158,7 @@ const Projects = () => {
       </BuyButtonContainer>
 
       <Scrollback
-        display={["block", "block", "none"]}
+        display={["block", "block", "block", "none"]}
         position="fixed"
         bottom={6}
         left={6}
