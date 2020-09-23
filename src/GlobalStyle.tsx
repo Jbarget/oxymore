818a3a8e254dab9ceb52fb0d677087102a28a8b1
sophie <sophieleila@icloud.com>
2020-09-23 14:36:16 +0200

    pr comments

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -12,7 +12,7 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     color: ${theme.colors.athensGray};
     background: ${theme.colors.black};
-    padding: 24px;
+    padding: ${theme.space[6]}px;
   }
   #root {
     height: 100%;
