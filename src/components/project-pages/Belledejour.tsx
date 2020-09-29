aecffa86e50f5dbd25b70f41dc0937b6953b9004
sssoja <53219789+sssoja@users.noreply.github.com>
2020-09-29 15:14:27 +0200

    creates timer to be used for all project pages #150 (#152)
    * creates timer to be used for all project pages
    
    Co-authored-by: jbarget <dear.justen@gmail.com>

--- src/components/project-pages/Belledejour.tsx
+++ src/components/project-pages/Belledejour.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import styled from "styled-components";
-import Timer from "./Timer";
+import Timer from "../Timer";
 import { FlexboxProps, flexbox } from "styled-system";
 
 const Main = styled.main<FlexboxProps>`
   display: flex;
+  height: 100vh;
   overflow: hidden;
-  height: 100%;
   ${flexbox}
 `;
 
-const Loading = () => {
+const Belledejour = () => {
   return (
     <Main justifyContent="center">
-      <Timer endDate="2020-09-30" />
+      <Timer endDate="2020-11-20" />
     </Main>
   );
 };
 
-export default Loading;
+export default Belledejour;
