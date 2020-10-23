3bb4d73db2d0f45c210d959d49107238ac1aeb99
jbarget <dear.justen@gmail.com>
2020-10-23 15:24:21 +0200

    fixes background across the app #183

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import theme from "./components/theme";
 import "./css/reset.css";
 import Header from "./components/Header";
-// import Loading from "./components/Loading";
+import Loading from "./components/Loading";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -13,52 +13,56 @@ import AboutUs from "./components/AboutUs";
 import Manifesto from "./components/Manifesto";
 import Advertising from "./components/Advertising";
 import ConsciousShopping from "./components/project-pages/ConsciousShopping";
-// import MarcMedina from "./components/project-pages/MarcMedina";
-// import Belledejour from "./components/project-pages/Belledejour";
-// import Eye from "./components/project-pages/Eye";
-// import LeoAdef from "./components/project-pages/LeoAdef";
-// import EroticStories from "./components/project-pages/EroticStories";
-// import Map from "./components/project-pages/Map";
-// import KaiLandre from "./components/project-pages/KaiLandre";
-// import FashionEditorial from "./components/project-pages/FashionEditorial";
+import MarcMedina from "./components/project-pages/MarcMedina";
+import Belledejour from "./components/project-pages/Belledejour";
+import Eye from "./components/project-pages/Eye";
+import LeoAdef from "./components/project-pages/LeoAdef";
+import EroticStories from "./components/project-pages/EroticStories";
+import Map from "./components/project-pages/Map";
+import KaiLandre from "./components/project-pages/KaiLandre";
+import FashionEditorial from "./components/project-pages/FashionEditorial";
+import Background from "./components/Background";
 
 const AppContent = styled.div`
-height: 100%;
-display: flex;
-flex-direction: column;
-`;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: ${theme.space[6]}px;
+  `;
 
 const App = () => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <AppContent>
-          <Suspense fallback={<div>Loading</div>}>
-             <Header /> 
-            <Switch>
-              {/* <Route path="/" exact component={Loading} /> */}
-             <Route path="/oxymore" exact component={Home} />
-              <Route path="/contact" exact component={Contact} />
-              <Route path="/manifesto" exact component={Manifesto} />
-              <Route path="/about" exact component={AboutUs} />
-              <Route path="/advertising" exact component={Advertising} />
-              <Route path="/projects" exact component={Projects} />
-              {/* <Route path="/projects/marcmedina" exact component={MarcMedina} /> */}
-              {/* <Route path="/projects/eroticstories" exact component={EroticStories} /> */}
-              {/* <Route path="/projects/eyes" exact component={Eye} /> */}
-              {/* <Route path="/projects/belledejour" exact component={Belledejour} /> */}
-              {/* <Route path="/projects/leoadef" exact component={LeoAdef} /> */}
-              {/* <Route path="/projects/themap" exact component={Map} /> */}
-              {/* <Route path="/projects/kailandre" exact component={KaiLandre} /> */}
-              {/* <Route path="/projects/editorial" exact component={FashionEditorial} /> */}
-              <Route
-                path="/projects/consciousshopping"
-                exact
-                component={ConsciousShopping}
-              />
-            </Switch>
-          </Suspense>
-        </AppContent>
+        <Background>
+          <AppContent>
+            <Suspense fallback={<div>Loading</div>}>
+              <Header />
+              <Switch>
+                <Route path="/" exact component={Loading} />
+                <Route path="/oxymore" exact component={Home} />
+                <Route path="/contact" exact component={Contact} />
+                <Route path="/manifesto" exact component={Manifesto} />
+                <Route path="/about" exact component={AboutUs} />
+                <Route path="/advertising" exact component={Advertising} />
+                <Route path="/projects" exact component={Projects} />
+                <Route path="/projects/marcmedina" exact component={MarcMedina} />
+                <Route path="/projects/eroticstories" exact component={EroticStories} />
+                <Route path="/projects/eyes" exact component={Eye} />
+                <Route path="/projects/belledejour" exact component={Belledejour} />
+                <Route path="/projects/leoadef" exact component={LeoAdef} />
+                <Route path="/projects/themap" exact component={Map} />
+                <Route path="/projects/kailandre" exact component={KaiLandre} />
+                <Route path="/projects/editorial" exact component={FashionEditorial} />
+                <Route
+                  path="/projects/consciousshopping"
+                  exact
+                  component={ConsciousShopping}
+                  />
+              </Switch>
+            </Suspense>
+          </AppContent>
+        </Background>
         <GlobalStyle />
       </ThemeProvider>
     </Router>
--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,26 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 import theme from "./components/theme";
-import background from "./components/assets/backgrounds/background.png";
-
 
 const GlobalStyle = createGlobalStyle`
-@font-face{
-  font-family: SangBleu;
-  src: url(SangBleuOGSerif-Light.otf)
-}
-  html {
-    height: 100%;
+  @font-face{
+    font-family: SangBleu;
+    src: url(SangBleuOGSerif-Light.otf)
   }
+
   body {
     min-height: 100%;
     height: 100%;
     font-family: ${theme.fonts.primary};
     text-decoration: none;
     color: ${theme.colors.athensGray};
-    background-image: url(${background});
-    background-repeat: repeat-y;
-    background-size: cover;
-    padding: ${theme.space[6]}px;
   }
   #root {
     height: 100%;
