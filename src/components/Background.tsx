ba2dcda159606925eceb4813e99779363d775bb0
Justen Barget <Jbarget@users.noreply.github.com>
2020-10-27 15:23:43 +0100

    fixes background across the app (#196)
    * fixes background across the app #183

---
+++ src/components/Background.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import marbleBackground from "./assets/backgrounds/background.png"
+
+type Background = "black" | typeof marbleBackground;
+
+const Div = styled.div<{ background: Background }>`
+  ${props => props.background === "black"
+    ? "background-color: black;"
+    : `background: url(${props.background});`
+  }
+  background-repeat: repeat-y;
+  background-size: cover;
+  height: 100%;
+`
+
+const getBackground = (pathname: string): Background => {
+  const pagesWithMarbleBG = [ "/", "/projects", "/manifesto", "/advertising", "/about", "/contact", "/buy", "/oxymore" ];
+  const pagesWithBlackBG = ["/projects/consciousshopping", "/projects/fashioneditorial", "/projects/kailandre", "/projects/eroticstories", "/projects/eyes", "/projects/themap", "/projects/belledejour", "/projects/leoadef"];
+
+  if (pagesWithBlackBG.includes(pathname)) {
+    return "black";
+  }
+  if (pagesWithMarbleBG.includes(pathname)) {
+    return marbleBackground;
+  }
+
+  return "black";
+}
+
+const Background: React.FC = ({ children }) => {
+  const location = useLocation();
+  const background = getBackground(location.pathname)
+
+  return (
+    <Div background={background}>
+      {children}
+    </Div>
+  )
+}
+
+export default Background;
