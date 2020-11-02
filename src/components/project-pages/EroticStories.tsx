74180a19825a2226e2532c9a7b2c98d7db76d292
jbarget <dear.justen@gmail.com>
2020-11-02 10:52:43 +0100

    tidies up erotic stories

--- src/components/project-pages/EroticStories.tsx
+++ src/components/project-pages/EroticStories.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styled from "styled-components";
-import { FlexboxProps, flexbox, layout, LayoutProps, grid, GridProps, space, SpaceProps, typography, TypographyProps } from "styled-system";
+import {
+  FlexboxProps,
+  flexbox,
+  layout,
+  LayoutProps,
+  grid,
+  GridProps,
+  space,
+  SpaceProps,
+  typography,
+  TypographyProps,
+} from "styled-system";
 import EroticStoriesPreview from "./EroticStoriesPreview";
 import alasImg from "./../assets/erotic-stories/alas-bateando.jpg";
 import ombligosImg from "./../assets/erotic-stories/ombligos.jpg";
 import { useTranslation } from "react-i18next";
-
 
 const Main = styled.main<TypographyProps>`
   ${typography};
@@ -24,37 +34,23 @@ const H1 = styled.h1<TypographyProps & SpaceProps>`
   ${typography};
 `;
 
-const H2 = styled.h1<TypographyProps & SpaceProps>`
+const H2 = styled.h2<TypographyProps & SpaceProps>`
   ${space};
   ${typography};
 `;
 
-
-const EroticImageContainer = styled.div<GridProps & SpaceProps>`
-  ${grid};
-  ${space};
+const EroticImageContainer = styled.div<GridProps>`
   object-fit: contain;
   height: fit-content;
-`;
-
-const EroticImage = styled.img<SpaceProps>`
-height: 100%;
-width: 100%;
-${space};
-`;
-
-const ScrollableTextContainer = styled.div<GridProps & SpaceProps>`
   ${grid};
-  ${space};
-  object-fit: contain;
-  overflow: scroll;
 `;
 
+const EroticImage = styled.img``;
 
-const ScrollableText = styled.p`
+const ScrollableText = styled.p<GridProps>`
   overflow: scroll;
+  ${grid};
 `;
-
 
 interface EroticStoryProps {
   title: string;
@@ -63,9 +59,6 @@ interface EroticStoryProps {
   alt: string;
 }
 
-
-
-
 const EroticStories = () => {
   const { t } = useTranslation();
   const eroticStories: EroticStoryProps[] = [
@@ -73,31 +66,34 @@ const EroticStories = () => {
       title: "Ombligos",
       img: ombligosImg,
       text: t("erotic-stories.poem"),
-      alt: "Artwork image",
+      alt: "Ombligos image",
     },
     {
       title: "Alas Bateando",
       img: ombligosImg,
       text: t("erotic-stories.poem"),
-      alt: "Artwork image",
-    }
+      alt: "Alas Bateando image",
+    },
   ];
 
-const eroticStory1 = eroticStories[0]
-const eroticStory2 = eroticStories[1]
+  const eroticStory1 = eroticStories[0];
+  const eroticStory2 = eroticStories[1];
 
   return (
     <Main fontFamily="secondary" textAlign="justify">
       <H1 fontSize={5} mb={4}>
-       Erotic Stories
+        Erotic Stories
       </H1>
       <H2 fontSize={5} mb={4}>
         my wings are beating
       </H2>
-      <Grid display={["flex", "flex", "flex", "grid"]}
-        height = {["auto", "auto", "auto", "100%"]}
+      <Grid
+        display="grid"
+        height="100%"
         flexDirection="column"
         justifyContent="center"
+        gridRowGap={3}
+        gridColumnGap={3}
         gridTemplateColumns={[
           "repeat(1, 100% [col-start])",
           "repeat(1, 100% [col-start])",
@@ -105,24 +101,18 @@ const eroticStory2 = eroticStories[1]
           "repeat(2, 50% [col-start])",
         ]}
       >
-      <EroticImageContainer gridColumn={1} pb={4} pr={[0, 0, 0, 4]} >
-      <EroticImage src={eroticStory1.img} alt={eroticStory1.alt}/>
-      </EroticImageContainer>
-      <ScrollableTextContainer gridColumn={2} pb={4} pl={[0, 0, 0, 4]}>
-      <ScrollableText >
-        {eroticStories[0].text}
-      </ScrollableText>
-      </ScrollableTextContainer>
-      <EroticImageContainer gridColumn={2} pt={4} pl={[0, 0, 0, 4]}>
-      <EroticImage src={eroticStory2.img} alt={eroticStory2.alt}/>
-      </EroticImageContainer>
-      <ScrollableTextContainer gridColumn={1} gridRow={2} pb={8} pt={4} pr={[0, 0, 0, 4]}>
-      <ScrollableText>
-        {eroticStories[1].text}
-      </ScrollableText>
-      </ScrollableTextContainer>
+        <EroticImageContainer>
+          <EroticImage src={eroticStory1.img} alt={eroticStory1.alt} />
+        </EroticImageContainer>
+        <ScrollableText>{eroticStory1.text}</ScrollableText>
+        <EroticImageContainer gridColumn={2}>
+          <EroticImage src={eroticStory2.img} alt={eroticStory2.alt} />
+        </EroticImageContainer>
+        <ScrollableText gridColumn={1} gridRow={2}>
+          {eroticStory2.text}
+        </ScrollableText>
       </Grid>
-        {/* <EroticStoriesPreview/> */}
+      {/* <EroticStoriesPreview/> */}
     </Main>
   );
 };
