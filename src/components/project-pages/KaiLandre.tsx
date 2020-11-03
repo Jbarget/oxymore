4c61f7b5d018ab45990bc55de0a47c6d582b0c08
sophie <sophieleila@icloud.com>
2020-11-03 15:23:32 +0100

    removes unused React hooks

--- src/components/project-pages/KaiLandre.tsx
+++ src/components/project-pages/KaiLandre.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { FlexboxProps, flexbox, layout, LayoutProps } from "styled-system";
 import KaiLandrePreview from "./KaiLandrePreview";
