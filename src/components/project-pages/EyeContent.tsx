ad268ae66b454ebc27d8fd537ba77ffce8249b6e
jbarget <dear.justen@gmail.com>
2020-11-25 12:34:49 +0000

    wip 2

--- src/components/project-pages/EyeContent.tsx
+++ src/components/project-pages/EyeContent.tsx
@@ -1,50 +1,85 @@
 import React from "react";
 import styled from "styled-components";
 import { grid, GridProps, layout, LayoutProps } from "styled-system";
-import { EyeData } from "../../fixtures/eye-data";
-import eye from "../assets/project-page/eye.png";
-import Flex from "../Flex";
+import { DataValues, EyeData } from "../../fixtures/eye-data";
+import eyeBlue from "../assets/eye/eye_blue.png";
+import eyeGray from "../assets/eye/eye_gray.png";
 import Grid from "../Grid";
 
 import data from "../../fixtures/eye-data";
 
-const Img = styled.img<LayoutProps>`
-  ${layout};
-`;
+const defaultMaximumValues = {
+  population: 0,
+  populationAffected: 0,
+  percentageAffected: 0,
+};
 
-const GridItem = styled.div<GridProps>`
+const maximumValues: DataValues = data.reduce((accum, eyeData) => {
+  return {
+    population:
+      accum.population > eyeData.data.population
+        ? accum.population
+        : eyeData.data.population,
+    populationAffected:
+      accum.populationAffected > eyeData.data.populationAffected
+        ? accum.populationAffected
+        : eyeData.data.populationAffected,
+    percentageAffected:
+      accum.percentageAffected > eyeData.data.percentageAffected
+        ? accum.percentageAffected
+        : eyeData.data.percentageAffected,
+  };
+}, defaultMaximumValues);
+
+const getRelativeValues = ({
+  population,
+  populationAffected,
+  percentageAffected,
+}: DataValues): DataValues => {
+  const values = {
+    population: population / maximumValues.population,
+    populationAffected: populationAffected / maximumValues.populationAffected,
+    percentageAffected: percentageAffected / maximumValues.percentageAffected,
+  };
+  return values;
+};
+
+const GridImg = styled.img<GridProps & LayoutProps>`
+  max-width: 100%;
+  ${layout};
   ${grid};
 `;
 
+const GridItem = styled.div<GridProps & LayoutProps>`
+  ${grid};
+  ${layout};
+`;
+
 const DataPoint: React.FC<EyeData> = ({ name, coords, data }) => {
+  const relativeValues = getRelativeValues(data);
+  console.log(relativeValues.population);
+
   return (
     <GridItem
       key={name}
-      gridArea={`${coords[0]} / ${coords[1]} / span 100 / span 100`}
+      gridRow={coords[0]}
+      gridColumn={coords[1]}
+      width="20px"
     >
-      <Img src={eye} alt="eye icon" maxWidth={data.population} />
+      <GridImg src={eyeBlue} alt={name} />
     </GridItem>
   );
 };
 const EyeContent = () => {
   return (
-    <Flex
+    <Grid
       height="100%"
-      width="100%"
-      overflow="hidden"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
+      width="60%"
+      gridTemplateRows="repeat(180, minmax(0, auto))"
+      gridTemplateColumns="repeat(120, minmax(0, auto))"
     >
-      <Grid
-        height="100%"
-        width="50%"
-        gridTemplateRows="repeat(220, 1fr)"
-        gridTemplateColumns="repeat(180, 1fr)"
-      >
-        {data.map(DataPoint)}
-      </Grid>
-    </Flex>
+      {data.map(DataPoint)}
+    </Grid>
   );
 };
 
--- src/fixtures/eye-data.ts
+++ src/fixtures/eye-data.ts
@@ -1,12 +1,14 @@
+export interface DataValues {
+  population: number;
+  populationAffected: number;
+  percentageAffected: number;
+}
+
 export interface EyeData {
   coords: [number, number];
   name: string;
   active: boolean;
-  data: {
-    population: number;
-    populationPerCapita: number;
-    percentage: number;
-  };
+  data: DataValues;
 }
 
 const eyeData: EyeData[] = [
@@ -15,9 +17,9 @@ const eyeData: EyeData[] = [
     coords: [17, 68],
     active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 36444324,
+      populationAffected: 1382808,
+      percentageAffected: 4.03,
     },
   },
   {
@@ -25,9 +27,9 @@ const eyeData: EyeData[] = [
     coords: [18, 105],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 11835284,
+      populationAffected: 514191,
+      percentageAffected: 4,
     },
   },
   {
@@ -35,9 +37,9 @@ const eyeData: EyeData[] = [
     coords: [26, 133],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 6700086,
+      populationAffected: 147514,
+      percentageAffected: 2.9,
     },
   },
   {
@@ -45,9 +47,9 @@ const eyeData: EyeData[] = [
     coords: [29, 158],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 100517804,
+      populationAffected: 2612803,
+      percentageAffected: 3.55,
     },
   },
   {
@@ -55,9 +57,19 @@ const eyeData: EyeData[] = [
     coords: [36, 95],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 43007769,
+      populationAffected: 1425342,
+      percentageAffected: 3.75,
+    },
+  },
+  {
+    name: "Mauritius",
+    coords: [36, 95],
+    active: false,
+    data: {
+      population: 4573157,
+      populationAffected: 107317,
+      percentageAffected: 4.34,
     },
   },
   {
@@ -65,9 +77,9 @@ const eyeData: EyeData[] = [
     coords: [52, 152],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 45308399,
+      populationAffected: 1124066,
+      percentageAffected: 4.55,
     },
   },
   {
@@ -75,19 +87,19 @@ const eyeData: EyeData[] = [
     coords: [55, 127],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 16431332,
+      populationAffected: 409439,
+      percentageAffected: 5.75,
     },
   },
   {
     name: "Niger",
     coords: [56, 101],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 24314931,
+      populationAffected: 620589,
+      percentageAffected: 5.69,
     },
   },
   {
@@ -95,29 +107,29 @@ const eyeData: EyeData[] = [
     coords: [55, 75],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 20456890,
+      populationAffected: 414692,
+      percentageAffected: 4.75,
     },
   },
   {
     name: "Senegal",
     coords: [54, 45],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 17487409,
+      populationAffected: 329943,
+      percentageAffected: 3.99,
     },
   },
   {
     name: "Cape Verde",
     coords: [55, 22],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 552850,
+      populationAffected: 14611,
+      percentageAffected: 3.72,
     },
   },
   {
@@ -125,9 +137,9 @@ const eyeData: EyeData[] = [
     coords: [57, 174],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 5891500,
+      populationAffected: 180704,
+      percentageAffected: 6.46,
     },
   },
   {
@@ -136,8 +148,8 @@ const eyeData: EyeData[] = [
     active: false,
     data: {
       population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      populationAffected: 2,
+      percentageAffected: 3,
     },
   },
   {
@@ -145,9 +157,9 @@ const eyeData: EyeData[] = [
     coords: [71, 164],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 111971051,
+      populationAffected: 2348694,
+      percentageAffected: 3.96,
     },
   },
   {
@@ -155,9 +167,9 @@ const eyeData: EyeData[] = [
     coords: [74, 93],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 12360726,
+      populationAffected: 236187,
+      percentageAffected: 4.13,
     },
   },
   {
@@ -165,19 +177,19 @@ const eyeData: EyeData[] = [
     coords: [67, 79],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 20860541,
+      populationAffected: 406485,
+      percentageAffected: 4.47,
     },
   },
   {
     name: "Guinea",
     coords: [70, 59],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 14354833,
+      populationAffected: 339620,
+      percentageAffected: 4.61,
     },
   },
   {
@@ -185,9 +197,9 @@ const eyeData: EyeData[] = [
     coords: [70, 45],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 2068363,
+      populationAffected: 50326,
+      percentageAffected: 4.6,
     },
   },
   {
@@ -196,18 +208,18 @@ const eyeData: EyeData[] = [
     active: false,
     data: {
       population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      populationAffected: 2,
+      percentageAffected: 3,
     },
   },
   {
     name: "Sierra Leone",
     coords: [80, 53],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 7160373,
+      populationAffected: 143695,
+      percentageAffected: 4.31,
     },
   },
   {
@@ -215,19 +227,19 @@ const eyeData: EyeData[] = [
     coords: [84, 63],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 5090855,
+      populationAffected: 109345,
+      percentageAffected: 4.25,
     },
   },
   {
     name: "Ghana",
     coords: [83, 79],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 30530449,
+      populationAffected: 634228,
+      percentageAffected: 3.86,
     },
   },
   {
@@ -235,9 +247,9 @@ const eyeData: EyeData[] = [
     coords: [80, 89],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 8293638,
+      populationAffected: 163297,
+      percentageAffected: 4.13,
     },
   },
   {
@@ -245,9 +257,9 @@ const eyeData: EyeData[] = [
     coords: [80, 106],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 206863087,
+      populationAffected: 3904999,
+      percentageAffected: 4.13,
     },
   },
   {
@@ -255,9 +267,9 @@ const eyeData: EyeData[] = [
     coords: [81, 130],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 5408758,
+      populationAffected: 142693,
+      percentageAffected: 4.65,
     },
   },
   {
@@ -265,29 +277,29 @@ const eyeData: EyeData[] = [
     coords: [81, 183],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 12422741,
+      populationAffected: 279139,
+      percentageAffected: 4.84,
     },
   },
   {
     name: "Kenya",
     coords: [93, 168],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 52186722,
+      populationAffected: 750760,
+      percentageAffected: 3.02,
     },
   },
   {
     name: "Uganda",
     coords: [86, 151],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 45856367,
+      populationAffected: 536641,
+      percentageAffected: 2.99,
     },
   },
   {
@@ -295,9 +307,9 @@ const eyeData: EyeData[] = [
     coords: [89, 117],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 26332965,
+      populationAffected: 362539,
+      percentageAffected: 2.89,
     },
   },
   {
@@ -305,19 +317,19 @@ const eyeData: EyeData[] = [
     coords: [95, 111],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 970828,
+      populationAffected: 18649,
+      percentageAffected: 3.3,
     },
   },
   {
     name: "Gabon",
     coords: [104, 114],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 1917169,
+      populationAffected: 37584,
+      percentageAffected: 2.91,
     },
   },
   {
@@ -325,9 +337,9 @@ const eyeData: EyeData[] = [
     coords: [105, 125],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 5263342,
+      populationAffected: 83782,
+      percentageAffected: 2.99,
     },
   },
   {
@@ -335,9 +347,9 @@ const eyeData: EyeData[] = [
     coords: [98, 135],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 90169404,
+      populationAffected: 1686670,
+      percentageAffected: 3.98,
     },
   },
   {
@@ -345,9 +357,9 @@ const eyeData: EyeData[] = [
     coords: [99, 147],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 12996594,
+      populationAffected: 191276,
+      percentageAffected: 2.96,
     },
   },
   {
@@ -355,19 +367,19 @@ const eyeData: EyeData[] = [
     coords: [104, 145],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 13126273,
+      populationAffected: 194434,
+      percentageAffected: 3.31,
     },
   },
   {
-    name: "Tanzania",
+    name: "United Republic of Tanzania",
     coords: [106, 159],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 62267349,
+      populationAffected: 895117,
+      percentageAffected: 3.02,
     },
   },
   {
@@ -375,9 +387,9 @@ const eyeData: EyeData[] = [
     coords: [115, 178],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 883162,
+      populationAffected: 14412,
+      percentageAffected: 3.27,
     },
   },
   {
@@ -385,19 +397,19 @@ const eyeData: EyeData[] = [
     coords: [118, 139],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 18882333,
+      populationAffected: 227777,
+      percentageAffected: 2.81,
     },
   },
   {
     name: "Angola",
     coords: [122, 124],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 29245334,
+      populationAffected: 383947,
+      percentageAffected: 3.21,
     },
   },
   {
@@ -405,29 +417,29 @@ const eyeData: EyeData[] = [
     coords: [121, 151],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 20022268,
+      populationAffected: 326529,
+      percentageAffected: 3.47,
     },
   },
   {
     name: "Mozambique",
     coords: [128, 161],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 31992997,
+      populationAffected: 514011,
+      percentageAffected: 3.26,
     },
   },
   {
     name: "Zimbabwe",
     coords: [135, 148],
-    active: false,
+    active: true,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 17470705,
+      populationAffected: 184147,
+      percentageAffected: 2.37,
     },
   },
   {
@@ -435,9 +447,9 @@ const eyeData: EyeData[] = [
     coords: [141, 135],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 2460223,
+      populationAffected: 19859,
+      percentageAffected: 1.37,
     },
   },
   {
@@ -445,9 +457,9 @@ const eyeData: EyeData[] = [
     coords: [135, 120],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 2731165,
+      populationAffected: 39950,
+      percentageAffected: 2.62,
     },
   },
   {
@@ -455,9 +467,9 @@ const eyeData: EyeData[] = [
     coords: [137, 192],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 27798964,
+      populationAffected: 484014,
+      percentageAffected: 3.47,
     },
   },
   {
@@ -465,9 +477,9 @@ const eyeData: EyeData[] = [
     coords: [149, 151],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 1366266,
+      populationAffected: 17559,
+      percentageAffected: 2.46,
     },
   },
   {
@@ -475,9 +487,9 @@ const eyeData: EyeData[] = [
     coords: [155, 145],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 2257685,
+      populationAffected: 37091,
+      percentageAffected: 2.98,
     },
   },
   {
@@ -485,9 +497,9 @@ const eyeData: EyeData[] = [
     coords: [165, 136],
     active: false,
     data: {
-      population: 34,
-      populationPerCapita: 2,
-      percentage: 3,
+      population: 56668602,
+      populationAffected: 984002,
+      percentageAffected: 2.31,
     },
   },
 ];
