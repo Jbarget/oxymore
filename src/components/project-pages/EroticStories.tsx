3f30ac64d243203d51dc7344f78bf2afdb4b8a5d
sssoja <sophie@sssoja.com>
2020-12-10 17:49:16 +0100

    adds spanish translation of erotic story

--- src/components/project-pages/EroticStories.tsx
+++ src/components/project-pages/EroticStories.tsx
@@ -11,7 +11,7 @@ const Main = styled(Flex)<TypographyProps>`
   ${typography};
 `;
 
-const launchDate = "2021-01-21";
+const launchDate = "2020-01-21";
 const EroticStories = () => {
   return (
     <Main
