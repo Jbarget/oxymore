8521ce20372b084773ce814d0fcba35f66b1eee4
sssoja <53219789+sssoja@users.noreply.github.com>
2020-12-10 19:05:33 +0100

    updates timer to new launch date (#289)

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-
 import Flex from "./Flex";
+import React from "react";
 import Timer from "./Timer";
 
 const Loading = () => {
   return (
     <Flex flex="auto" overflow="hidden" justifyContent="center">
-      <Timer launchDate="2020-12-11" />
+      <Timer launchDate="2021-01-08" />
     </Flex>
   );
 };
