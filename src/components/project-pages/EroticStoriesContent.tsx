637e5bf66121f95e379ef36a3224540246266a2c
sssoja <sophie@sssoja.com>
2020-12-16 12:14:20 +0100

    finalises erotic stories page

--- src/components/project-pages/EroticStoriesContent.tsx
+++ src/components/project-pages/EroticStoriesContent.tsx
@@ -10,9 +10,11 @@ import {
   space,
   typography,
 } from "styled-system";
-import React, { Fragment } from "react";
 
+import { Link } from "react-router-dom";
+import React from "react";
 import bellyButtonsImg from "../assets/erotic-stories/belly-buttons.jpg";
+import statue from "../assets/project-page/statue.png";
 import styled from "styled-components";
 import { useTranslation } from "react-i18next";
 import wingsBeatingImg from "../assets/erotic-stories/wings-beating.jpg";
@@ -21,6 +23,15 @@ const Grid = styled.div<GridProps & FlexboxProps & LayoutProps>`
   ${grid};
   ${flexbox};
   ${layout};
+`;
+
+const GridSquare = styled(Grid)<SpaceProps>`
+  ${space};
+`;
+
+const ProjectIcon = styled.img<LayoutProps & FlexboxProps>`
+  ${layout};
+  ${flexbox};
 `;
 
 const H1 = styled.h1<TypographyProps & SpaceProps>`
@@ -34,15 +45,16 @@ const H2 = styled.h2<TypographyProps & SpaceProps>`
   ${typography};
 `;
 
-const EroticImageContainer = styled.div<GridProps>`
-  ${grid};
+const Div = styled.div<FlexboxProps & LayoutProps & SpaceProps>`
+  ${flexbox};
+  ${layout};
+  ${space};
 `;
 
-const EroticImage = styled.img``;
-
-const ScrollableText = styled.p<GridProps>`
+const ScrollableText = styled.p<GridProps & TypographyProps>`
   overflow: scroll;
   ${grid};
+  ${typography};
 `;
 
 interface EroticStoryProps {
@@ -70,43 +82,62 @@ const EroticStoriesContent = () => {
   const bellyButtons = eroticStories[1];
 
   return (
-    <Fragment>
-      <H1 fontSize={5} mb={4}>
-        Erotic Stories
-      </H1>
-      <H2 fontSize={5} mb={4}>
-        my wings are beating
-      </H2>
-      <Grid
-        display="grid"
-        height={["auto", "auto", "100%"]}
-        flexDirection="column"
-        justifyContent="center"
-        gridRowGap={3}
-        gridColumnGap={3}
-        gridTemplateColumns={[
-          "repeat(1, 1fr)",
-          "repeat(1, 1fr)",
-          "repeat(2, 1fr)",
-        ]}
-      >
-        <EroticImageContainer>
-          <EroticImage src={wingsBeating.img} alt={wingsBeating.alt} />
-        </EroticImageContainer>
-        <ScrollableText
-          dangerouslySetInnerHTML={{ __html: wingsBeating.text }}
-        />
+    <Grid
+      display="grid"
+      height={["auto", "auto", "100%"]}
+      flexDirection="column"
+      justifyContent="center"
+      gridRowGap={3}
+      gridColumnGap={3}
+      gridTemplateColumns={[
+        "repeat(1, 1fr)",
+        "repeat(1, 1fr)",
+        "repeat(2, 1fr)",
+      ]}
+    >
+      <GridSquare gridColumn={1} gridRow={1} px={[0, 0, 4]}>
+        <Div display="flex" justifyContent="center" p={4}>
+          <Link to="/projects">
+            <ProjectIcon
+              src={statue}
+              alt="project icon"
+              maxWidth="15%"
+            ></ProjectIcon>
+          </Link>
+        </Div>
+        <H1 fontSize={[2, 3, 4, 5]} mb={4}>
+          Erotic Stories
+        </H1>
 
-        <EroticImageContainer gridRow={[3, 3, 2]} gridColumn={[1, 1, 2]}>
-          <EroticImage src={bellyButtons.img} alt={bellyButtons.alt} />
-        </EroticImageContainer>
+        <H2 fontSize={[2, 3, 4, 5]} mb={4}>
+          {t("erotic-stories.wingsTitle")}
+        </H2>
         <ScrollableText
           gridColumn={1}
+          gridRow={1}
+          textAlign="left"
+          dangerouslySetInnerHTML={{ __html: wingsBeating.text }}
+        />
+      </GridSquare>
+      <GridSquare gridColumn={[1, 1, 1, 2]} gridRow={[2, 2, 1]}>
+        <img src={wingsBeating.img} alt={wingsBeating.alt} />
+      </GridSquare>
+
+      <GridSquare gridColumn={1} gridRow={[4, 4, 2]} px={[0, 0, 4]}>
+        <img src={bellyButtons.img} alt={bellyButtons.alt} />
+      </GridSquare>
+      <GridSquare>
+        <H2 fontSize={[2, 3, 4, 5]} mb={4}>
+          {t("erotic-stories.bellyButtonsTitle")}
+        </H2>
+        <ScrollableText
+          gridColumn={[1, 1, 1, 2]}
           gridRow={2}
+          textAlign="left"
           dangerouslySetInnerHTML={{ __html: bellyButtons.text }}
         />
-      </Grid>
-    </Fragment>
+      </GridSquare>
+    </Grid>
   );
 };
 
