1093c7f2d0ca15a8306b4422808fc37a77fc6bf8
sssoja <sophie@sssoja.com>
2020-12-16 17:44:46 +0100

    adds grid rows

--- src/components/project-pages/EroticStoriesContent.tsx
+++ src/components/project-pages/EroticStoriesContent.tsx
@@ -25,8 +25,14 @@ const Grid = styled.div<GridProps & FlexboxProps & LayoutProps>`
   ${layout};
 `;
 
+const GridRow = styled.div<LayoutProps & GridProps>`
+  ${layout};
+  ${grid};
+`;
+
 const GridSquare = styled(Grid)<SpaceProps>`
   ${space};
+  scrollbar-color: transparent;
 `;
 
 const ProjectIcon = styled.img<LayoutProps & FlexboxProps>`
@@ -45,7 +51,7 @@ const H2 = styled.h2<TypographyProps & SpaceProps>`
   ${typography};
 `;
 
-const Div = styled.div<FlexboxProps & LayoutProps & SpaceProps>`
+const IconContainer = styled.div<FlexboxProps & LayoutProps & SpaceProps>`
   ${flexbox};
   ${layout};
   ${space};
@@ -81,67 +87,65 @@ const EroticStoriesContent = () => {
   const bellyButtons = eroticStories[1];
 
   return (
-    <Grid
-      display="grid"
-      height={["auto", "auto", "100%"]}
-      flexDirection="column"
-      justifyContent="center"
-      gridRowGap={3}
-      gridColumnGap={3}
-      gridTemplateColumns={[
-        "repeat(1, 1fr)",
-        "repeat(1, 1fr)",
-        "repeat(2, 1fr)",
-      ]}
-    >
-      <GridSquare
-        gridColumn={1}
-        gridRow={1}
-        px={[0, 0, 4]}
-        height="140vh"
+    <Grid gridRowGap={3}>
+      <GridRow
+        gridColumnGap={3}
+        gridTemplateColumns={[
+          "repeat(1, 1fr)",
+          "repeat(1, 1fr)",
+          "repeat(2, 1fr)",
+        ]}
+        height="100vh"
         overflow="scroll"
       >
-        <Div display="flex" justifyContent="center" p={4}>
-          <Link to="/projects">
-            <ProjectIcon
-              src={statue}
-              alt="project icon"
-              maxWidth="15%"
-            ></ProjectIcon>
-          </Link>
-        </Div>
-        <H1 fontSize={[2, 3, 4, 5]} mb={4}>
-          Erotic Stories
-        </H1>
-
-        <H2 fontSize={[2, 3, 4, 5]} mb={4}>
-          {t("erotic-stories.wingsTitle")}
-        </H2>
-        <ScrollableText
-          gridColumn={1}
-          gridRow={1}
-          textAlign="left"
-          dangerouslySetInnerHTML={{ __html: wingsBeating.text }}
-        />
-      </GridSquare>
-      <GridSquare gridColumn={[1, 1, 1, 2]} gridRow={[2, 2, 1]}>
-        <img src={wingsBeating.img} alt={wingsBeating.alt} />
-      </GridSquare>
-
-      <GridSquare gridColumn={1} gridRow={[4, 4, 2]} px={[0, 0, 4]}>
-        <img src={bellyButtons.img} alt={bellyButtons.alt} />
-      </GridSquare>
-      <GridSquare height="140vh" overflow="scroll">
-        <H2 fontSize={[2, 3, 4, 5]} mb={4}>
-          {t("erotic-stories.bellyButtonsTitle")}
-        </H2>
-        <ScrollableText
-          gridColumn={[1, 1, 1, 2]}
-          gridRow={2}
-          textAlign="left"
-          dangerouslySetInnerHTML={{ __html: bellyButtons.text }}
-        />
-      </GridSquare>
+        <GridSquare gridColumn={1}>
+          <IconContainer p={4}>
+            <Link to="/projects">
+              <ProjectIcon
+                src={statue}
+                alt="project icon"
+                maxWidth="10%"
+              ></ProjectIcon>
+            </Link>
+          </IconContainer>
+          <H1 fontSize={[2, 3, 4, 5]} mb={4}>
+            Erotic Stories
+          </H1>
+          <H2 fontSize={[2, 3, 4, 5]} mb={4}>
+            {t("erotic-stories.wingsTitle")}
+          </H2>
+          <ScrollableText
+            textAlign="left"
+            dangerouslySetInnerHTML={{ __html: wingsBeating.text }}
+          />
+        </GridSquare>
+        <GridSquare gridColumn={[1, 1, 2, 2]} py={[2, 2, 2, 0]}>
+          <img src={wingsBeating.img} alt={wingsBeating.alt} />
+        </GridSquare>
+      </GridRow>
+      <GridRow
+        gridColumnGap={3}
+        gridTemplateColumns={[
+          "repeat(1, 1fr)",
+          "repeat(1, 1fr)",
+          "repeat(2, fr)",
+        ]}
+        height="100vh"
+        overflow="scroll"
+      >
+        <GridSquare gridColumn={1} py={[2, 2, 2, 0]}>
+          <img src={bellyButtons.img} alt={bellyButtons.alt} />
+        </GridSquare>
+        <GridSquare gridColumn={[1, 1, 1, 2]}>
+          <H2 fontSize={[2, 3, 4, 5]} mb={4}>
+            {t("erotic-stories.bellyButtonsTitle")}
+          </H2>
+          <ScrollableText
+            textAlign="left"
+            dangerouslySetInnerHTML={{ __html: bellyButtons.text }}
+          />
+        </GridSquare>
+      </GridRow>
     </Grid>
   );
 };
