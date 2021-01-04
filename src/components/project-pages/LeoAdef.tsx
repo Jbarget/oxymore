00983c604f91dcb2f45dcccf36c79bb85f45618e
sssoja <53219789+sssoja@users.noreply.github.com>
2021-01-04 16:16:53 +0000

    creates leo adef page (#312)
    * creates leo adef page

--- src/components/project-pages/LeoAdef.tsx
+++ src/components/project-pages/LeoAdef.tsx
@@ -1,47 +1,31 @@
-import React, { Fragment } from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { layout, LayoutProps } from "styled-system";
+import { TypographyProps, typography } from "styled-system";
 
-import Timer from "../Timer";
-import spider from "../assets/project-page/spider.png";
-import PreviewOrProjectPage from "./PreviewOrProjectPage";
 import Flex from "../Flex";
-import { PROJECTS_URL } from "../../constants/router-urls";
+import LeoAdefContent from "./LeoAdefContent";
+import LeoAdefPreview from "./LeoAdefPreview";
+import PreviewOrProjectPage from "./PreviewOrProjectPage";
+import React from "react";
+import styled from "styled-components";
 
-const Img = styled.img<LayoutProps>`
-  ${layout};
+const Main = styled(Flex)<TypographyProps>`
+  ${typography};
 `;
 
-const PreviewPage: React.FC<{ launchDate: string }> = ({ launchDate }) => {
-  return (
-    <Fragment>
-      <Link to={PROJECTS_URL}>
-        <Flex justifyContent="center" alignItems="center">
-          <Img src={spider} alt="spider icon" maxWidth="30%" />
-        </Flex>
-      </Link>
-      <Timer launchDate={launchDate} />
-    </Fragment>
-  );
-};
-
-const launchDate = "2021-01-21";
+const launchDate = "2021-03-21";
 const LeoAdef = () => {
   return (
-    <Flex
+    <Main
       flex="auto"
-      overflow="hidden"
+      fontFamily="secondary"
+      textAlign="justify"
       flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
     >
       <PreviewOrProjectPage
         launchDate={launchDate}
-        PreviewPage={PreviewPage}
-        ProjectPage={() => null}
+        PreviewPage={LeoAdefPreview}
+        ProjectPage={LeoAdefContent}
       />
-    </Flex>
+    </Main>
   );
 };
 
