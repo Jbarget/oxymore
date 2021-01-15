d30cd9cd5beaa296440e3c12516f82dcd6783658
sssoja <sophie@sssoja.com>
2021-01-15 10:04:56 +0000

    adds erotic stories launch date

--- src/components/projects/erotic-stories/EroticStories.tsx
+++ src/components/projects/erotic-stories/EroticStories.tsx
@@ -11,7 +11,7 @@ const Main = styled(Flex)<TypographyProps>`
   ${typography};
 `;
 
-const launchDate = "2021-03-21";
+const launchDate = "2021-02-05";
 const EroticStories = () => {
   return (
     <Main
