0497a3e1baa8a2d9000cb30232789c79efadd1b9
sssoja <53219789+sssoja@users.noreply.github.com>
2021-01-21 16:42:12 +0000

    So/update link kai landre (#358)
    * adds route back to projects page
    
    * sets timer back to launch project at 13.00 cet

--- src/components/projects/kai-landre/KaiLandre.tsx
+++ src/components/projects/kai-landre/KaiLandre.tsx
@@ -1,23 +1,60 @@
+import { LayoutProps, PositionProps, layout, position } from "styled-system";
+import React, { Fragment } from "react";
+
 import Flex from "../../Flex";
 import KaiLandrePreview from "./KaiLandrePreview";
+import { Link } from "react-router-dom";
+import { PROJECTS_URL } from "../../../constants/router-urls";
 import PreviewOrProjectPage from "../PreviewOrProjectPage";
-import React from "react";
 import ReactPlayer from "react-player/lazy";
+import dragon from "../../../assets/project-page/project-icons/dragon.png";
+import styled from "styled-components";
+import theme from "../../theme";
 
-const KaiLandreContent: React.FC = () => {
+const ReturnToProjectsPage = styled(Link)<LayoutProps & PositionProps>`
+  ${layout};
+  ${position};
+  transition: transform 0.5s;
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
+
+const KaiLandreContent = () => {
   const url = "https://www.youtube.com/watch?v=AGSH_acR4wA";
 
   return (
-    <Flex flex="auto">
-      <ReactPlayer url={url} flex="auto" width="100%" />
-    </Flex>
+    <Fragment>
+      <Flex flex="auto" height={["60vh", "60vh", "60vh", "80vh"]}>
+        <ReactPlayer
+          url={url}
+          position="relative"
+          width="100%"
+          height="100%"
+          controls
+          playing
+          muted
+          zindex={theme.zIndexes.behind}
+        ></ReactPlayer>
+        <ReturnToProjectsPage
+          to={PROJECTS_URL}
+          width={80}
+          position="absolute"
+          right={60}
+          bottom={100}
+          display={["none", "none", "block", "block"]}
+        >
+          <img src={dragon} alt="dragon icon" />
+        </ReturnToProjectsPage>
+      </Flex>
+    </Fragment>
   );
 };
 
 const launchDate = "2021-01-22";
 const KaiLandre = () => {
   return (
-    <Flex flex="auto" justifyContent="center" alignItems="center">
+    <Flex flex="auto">
       <PreviewOrProjectPage
         launchDate={launchDate}
         PreviewPage={KaiLandrePreview}
--- src/helpers/calculateTimeLeft.ts
+++ src/helpers/calculateTimeLeft.ts
@@ -6,12 +6,12 @@ export interface TimeLeft {
 }
 
 const calculateTimeLeft = (date: string): TimeLeft => {
-  // +17 hours is so that the countdown ends at 6pm on the launchDate shown
+  // +12 hours is so that the countdown ends at +13 hours CET on the launchDate shown
   const hourInMilliseconds = 60 * 60 * 1000;
   const difference =
     Number(new Date(date).getTime()) -
     Number(new Date().getTime()) +
-    17 * hourInMilliseconds;
+    12 * hourInMilliseconds;
 
   const daysLeft = Math.floor(difference / (1000 * 60 * 60 * 24));
   const hoursLeft = Math.floor((difference / (1000 * 60 * 60)) % 24);
