8c3c14ca3d3c1dcae1191a79ad5c6e0171f6ccfc
sssoja <53219789+sssoja@users.noreply.github.com>
2021-01-22 09:48:23 +0000

    updates youtube url (#361)
    Approving this one as its time-sensitive feel free to have a gander at your leisure!

--- src/components/projects/kai-landre/KaiLandre.tsx
+++ src/components/projects/kai-landre/KaiLandre.tsx
@@ -1,11 +1,11 @@
 import { LayoutProps, PositionProps, layout, position } from "styled-system";
-import React, { Fragment } from "react";
 
 import Flex from "../../Flex";
 import KaiLandrePreview from "./KaiLandrePreview";
 import { Link } from "react-router-dom";
 import { PROJECTS_URL } from "../../../constants/router-urls";
 import PreviewOrProjectPage from "../PreviewOrProjectPage";
+import React from "react";
 import ReactPlayer from "react-player/lazy";
 import dragon from "../../../assets/project-page/project-icons/dragon.png";
 import styled from "styled-components";
@@ -21,33 +21,30 @@ const ReturnToProjectsPage = styled(Link)<LayoutProps & PositionProps>`
 `;
 
 const KaiLandreContent = () => {
-  const url = "https://www.youtube.com/watch?v=AGSH_acR4wA";
+  const url = "https://youtu.be/fjnXLRrHscM";
 
   return (
-    <Fragment>
-      <Flex flex="auto" height={["60vh", "60vh", "60vh", "80vh"]}>
-        <ReactPlayer
-          url={url}
-          position="relative"
-          width="100%"
-          height="100%"
-          controls
-          playing
-          muted
-          zindex={theme.zIndexes.behind}
-        ></ReactPlayer>
-        <ReturnToProjectsPage
-          to={PROJECTS_URL}
-          width={80}
-          position="absolute"
-          right={60}
-          bottom={100}
-          display={["none", "none", "block", "block"]}
-        >
-          <img src={dragon} alt="dragon icon" />
-        </ReturnToProjectsPage>
-      </Flex>
-    </Fragment>
+    <Flex flex="auto" height={["76vh", "76vh", "76vh", "80vh", "80vh"]}>
+      <ReactPlayer
+        url={url}
+        position="relative"
+        width="100%"
+        height="100%"
+        controls
+        playing
+        zindex={theme.zIndexes.behind}
+      ></ReactPlayer>
+      <ReturnToProjectsPage
+        to={PROJECTS_URL}
+        width={80}
+        position="absolute"
+        right={60}
+        bottom={100}
+        display={["none", "none", "none", "block"]}
+      >
+        <img src={dragon} alt="dragon icon" />
+      </ReturnToProjectsPage>
+    </Flex>
   );
 };
 
