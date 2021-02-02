46b1736f3d1e12c081abb04345e73b558c1a67da
sssoja <sophie@sssoja.com>
2021-02-02 14:16:27 +0000

    reduces grid width to prevent scroll on desktop

--- src/components/projects/eye/Eye.tsx
+++ src/components/projects/eye/Eye.tsx
@@ -1,38 +1,231 @@
-import EyeContent from "./EyeContent";
-import Flex from "../../Flex";
-import Icon from "../Icon";
-import PreviewOrProjectPage from "../PreviewOrProjectPage";
-import React from "react";
-import Timer from "../../Timer";
-import eye from "../../../assets/project-page/project-icons/eye.png";
+import "react-mobile-style-picker/dist/index.css";
 
-const PreviewPage: React.FC<{ launchDate: string }> = ({ launchDate }) => {
+import {
+  BorderProps,
+  GridProps,
+  LayoutProps,
+  TypographyProps,
+  border,
+  grid,
+  layout,
+  typography,
+} from "styled-system";
+import { DataSet, DataValues, EyeData } from "../../../fixtures/eye-data";
+import React, { useCallback, useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+import EyePreview from "./EyePreview";
+import Flex from "../../Flex";
+import Grid from "../../Grid";
+import { Picker } from "react-mobile-style-picker";
+import PreviewOrProjectPage from "../PreviewOrProjectPage";
+import data from "../../../fixtures/eye-data";
+import eyeBlue from "../../../assets/project-page/eye/eye_blue.png";
+import eyeBrown from "../../../assets/project-page/eye/eye_brown.png";
+
+const P = styled.p<BorderProps & TypographyProps>`
+  text-transform: uppercase;
+  font-family: SangBleu;
+  line-height: 28px;
+  ${border};
+  ${typography};
+`;
+
+const itemStyles: React.CSSProperties = {
+  color: "white",
+  fontFamily: "SangBleu",
+  textTransform: "uppercase",
+};
+
+const style: React.CSSProperties = {
+  height: "200px",
+};
+
+const defaultMaximumValues = {
+  population: 0,
+  populationAffected: 0,
+  percentageAffected: 0,
+};
+
+const maximumValues: DataValues = data.reduce((accum, eyeData) => {
+  return {
+    population:
+      accum.population > eyeData.data.population
+        ? accum.population
+        : eyeData.data.population,
+    populationAffected:
+      accum.populationAffected > eyeData.data.populationAffected
+        ? accum.populationAffected
+        : eyeData.data.populationAffected,
+    percentageAffected:
+      accum.percentageAffected > eyeData.data.percentageAffected
+        ? accum.percentageAffected
+        : eyeData.data.percentageAffected,
+  };
+}, defaultMaximumValues);
+
+const getRelativeValues = ({
+  population,
+  populationAffected,
+  percentageAffected,
+}: DataValues): DataValues => {
+  const values = {
+    population: population / maximumValues.population,
+    populationAffected: populationAffected / maximumValues.populationAffected,
+    percentageAffected: percentageAffected / maximumValues.percentageAffected,
+  };
+  return values;
+};
+
+const CountryData = ({ countryData }: { countryData: EyeData | null }) => {
+  if (!countryData) return null;
+  console.log(countryData);
+
   return (
     <Flex
-      flex="auto"
-      overflow="hidden"
+      position="fixed"
       flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
+      width="20%"
+      top={100}
+      right="5%"
     >
-      <Icon icon={eye}></Icon>
-      <Timer launchDate={launchDate} />
+      <P borderBottom="whiteThin" fontSize={3}>
+        {countryData.name}
+      </P>
+      <P fontSize={[1, 1, 2]}>Population: {countryData.data.population}</P>
+      <P fontSize={[1, 1, 2]}>
+        Population affected: {countryData.data.populationAffected}
+      </P>
+      <P fontSize={[1, 1, 2]}>
+        Percentage: {countryData.data.percentageAffected}
+      </P>
+      {countryData.data.casesTreated ? (
+        <P fontSize={[1, 1, 2]}>
+          Fundación Elena Barraquer: {countryData.data.casesTreated}
+        </P>
+      ) : null}
     </Flex>
   );
 };
-const launchDate = "2021-02-12";
+
+const bounce = keyframes`
+  0%, 100% {
+    -webkit-transform: translateY(0);
+  }
+  50% {
+    -webkit-transform: translateY(-5px);
+  }
+`;
+
+const GridImg = styled.img<GridProps & LayoutProps>`
+  max-width: 100%;
+  ${layout};
+  ${grid};
+`;
+
+const GridItem = styled.div<GridProps & LayoutProps>`
+  min-width: 0;
+  min-height: 0;
+  animation: ${bounce} 2s linear infinite;
+  cursor: pointer;
+  ${grid};
+  ${layout};
+`;
+
+const calculateSpanValue = ({
+  relativeValues,
+  selectedDataSet,
+}: {
+  relativeValues: DataValues;
+  selectedDataSet: DataSet;
+}) => {
+  const MINIMUM_SPAN_VALUE = 8;
+  const value = Math.ceil(relativeValues[selectedDataSet] * 20);
+
+  return value < MINIMUM_SPAN_VALUE ? MINIMUM_SPAN_VALUE : value;
+};
+interface DataPointProps extends EyeData {
+  selectedDataSet: DataSet;
+  onClick: () => void;
+}
+const DataPoint: React.FC<DataPointProps> = ({
+  name,
+  coords,
+  data,
+  selectedDataSet,
+  onClick,
+}) => {
+  const relativeValues = getRelativeValues(data);
+  const spanValue = calculateSpanValue({ relativeValues, selectedDataSet });
+  const eyeImage = data.casesTreated ? eyeBlue : eyeBrown;
+
+  return (
+    <GridItem
+      key={name}
+      gridRow={`${coords[0]} / span ${spanValue}`}
+      gridColumn={`${coords[1]} / span ${spanValue}`}
+      onClick={onClick}
+    >
+      <GridImg src={eyeImage} alt={name} />
+    </GridItem>
+  );
+};
+
+const EyeContent = () => {
+  const [
+    selectedCountryData,
+    setSelectedCountryData,
+  ] = useState<EyeData | null>(null);
+  const [selectedDataSet, setSelectedDataSet] = useState<DataSet>(
+    "percentageAffected"
+  );
+
+  const setCountryDetails = useCallback(
+    countryData => () => setSelectedCountryData(countryData),
+    []
+  );
+  return (
+    <Flex alignItems="center" flexDirection="column" position="relative">
+      <Grid
+        width={["85%", "85%", "85%", "60%", "38%"]}
+        gridTemplateRows="repeat(180, minmax(0, auto))"
+        gridTemplateColumns="repeat(120, minmax(0, auto))"
+      >
+        {data.map(datum => (
+          <DataPoint
+            {...datum}
+            selectedDataSet={selectedDataSet}
+            onClick={setCountryDetails(datum)}
+          />
+        ))}
+      </Grid>
+      <CountryData countryData={selectedCountryData} />
+      <Flex position="fixed" bottom={0} left={100}>
+        <Picker
+          onChange={setSelectedDataSet}
+          itemStyle={itemStyles}
+          style={style}
+          mask={false}
+          value={selectedDataSet}
+        >
+          <Picker.Item value="population">Population</Picker.Item>
+          <Picker.Item value="populationAffected">
+            Population Affected
+          </Picker.Item>
+          <Picker.Item value="percentageAffected">Percentage</Picker.Item>
+        </Picker>
+      </Flex>
+    </Flex>
+  );
+};
+
+const launchDate = "2020-02-12";
 const Eye = () => {
   return (
-    <Flex
-      flex="auto"
-      overflow="hidden"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
+    <Flex flex="auto">
       <PreviewOrProjectPage
         launchDate={launchDate}
-        PreviewPage={PreviewPage}
+        PreviewPage={EyePreview}
         ProjectPage={EyeContent}
       />
     </Flex>
