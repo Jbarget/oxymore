6c2074940bdea8a61075d10195dfe46788910b41
sssoja <sophie@sssoja.com>
2021-02-03 12:34:47 +0000

    adds route back to projects page

--- src/components/projects/eye/Eye.tsx
+++ src/components/projects/eye/Eye.tsx
@@ -1,35 +1,37 @@
 import "react-mobile-style-picker/dist/index.css";
 
+import { DataSet, DataValues, EyeData } from "../../../fixtures/eye-data";
 import {
-  BorderProps,
   GridProps,
   LayoutProps,
+  PositionProps,
   TypographyProps,
-  border,
   grid,
   layout,
+  position,
   typography,
 } from "styled-system";
-import { DataSet, DataValues, EyeData } from "../../../fixtures/eye-data";
-import React, { useCallback, useState } from "react";
+import React, { Fragment, useCallback, useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 import EyePreview from "./EyePreview";
 import Flex from "../../Flex";
 import Grid from "../../Grid";
+import { Link } from "react-router-dom";
+import { PROJECTS_URL } from "../../../constants/router-urls";
 import { Picker } from "react-mobile-style-picker";
 import PreviewOrProjectPage from "../PreviewOrProjectPage";
 import data from "../../../fixtures/eye-data";
+import eye from "../../../assets/project-page/project-icons/eye.png";
 import eyeBlue from "../../../assets/project-page/eye/eye_blue.png";
 import eyeBrown from "../../../assets/project-page/eye/eye_brown.png";
 import theme from "../../theme";
 import { useTranslation } from "react-i18next";
 
-const P = styled.p<BorderProps & TypographyProps>`
+const P = styled.p<TypographyProps>`
   text-transform: uppercase;
   font-family: ${theme.fonts.secondary};
   line-height: 28px;
-  ${border};
   ${typography};
 `;
 
@@ -124,7 +126,7 @@ const bounce = keyframes`
 
 const GridImg = styled.img<GridProps & LayoutProps>`
   max-width: 100%;
-  transition: transform 0.3s;
+  transition: transform 0.5s;
   &:hover {
     transform: scale(1.2);
   }
@@ -139,6 +141,15 @@ const GridItem = styled.div<GridProps & LayoutProps>`
   cursor: pointer;
   ${grid};
   ${layout};
+`;
+
+const ReturnToProjectsPage = styled(Link)<LayoutProps & PositionProps>`
+  ${layout};
+  ${position};
+  transition: transform 0.5s;
+  &:hover {
+    transform: scale(1.02);
+  }
 `;
 
 const calculateSpanValue = ({
@@ -194,46 +205,64 @@ const EyeContent = () => {
   const { t } = useTranslation();
 
   return (
-    <Flex alignItems="center" flexDirection="column" position="relative">
-      <Grid
-        width={[320, 320, 400, 540]}
-        gridTemplateRows="repeat(180, minmax(0, auto))"
-        gridTemplateColumns="repeat(120, minmax(0, auto))"
+    <Fragment>
+      <ReturnToProjectsPage
+        to={PROJECTS_URL}
+        width={80}
+        position="fixed"
+        left={60}
+        top={80}
+        zIndex={theme.zIndexes.inFront}
       >
-        {data.map(datum => (
-          <DataPoint
-            {...datum}
-            selectedDataSet={selectedDataSet}
-            onClick={setCountryDetails(datum)}
-          />
-        ))}
-      </Grid>
-      <CountryData countryData={selectedCountryData} />
-      <Flex position="fixed" bottom={0} left={50}>
-        <Picker
-          onChange={setSelectedDataSet}
-          itemStyle={itemStyles}
-          style={style}
-          mask={false}
-          value={selectedDataSet}
+        <img src={eye} alt="eye icon" />
+      </ReturnToProjectsPage>
+      <Flex
+        alignItems="center"
+        flexDirection="column"
+        position="relative"
+        justifyContent="center"
+        width="100%"
+      >
+        <Grid
+          width={[320, 320, 400, 540]}
+          gridTemplateRows="repeat(180, minmax(0, auto))"
+          gridTemplateColumns="repeat(120, minmax(0, auto))"
         >
-          <Picker.Item value="population">{t("eye.population")}</Picker.Item>
-          <Picker.Item value="populationAffected">
-            {t("eye.populationAffected")}
-          </Picker.Item>
-          <Picker.Item value="percentageAffected">
-            {t("eye.percentage")}
-          </Picker.Item>
-        </Picker>
+          {data.map(datum => (
+            <DataPoint
+              {...datum}
+              selectedDataSet={selectedDataSet}
+              onClick={setCountryDetails(datum)}
+            />
+          ))}
+        </Grid>
+        <CountryData countryData={selectedCountryData} />
+        <Flex position="fixed" bottom={0} left={50}>
+          <Picker
+            onChange={setSelectedDataSet}
+            itemStyle={itemStyles}
+            style={style}
+            mask={false}
+            value={selectedDataSet}
+          >
+            <Picker.Item value="population">{t("eye.population")}</Picker.Item>
+            <Picker.Item value="populationAffected">
+              {t("eye.populationAffected")}
+            </Picker.Item>
+            <Picker.Item value="percentageAffected">
+              {t("eye.percentage")}
+            </Picker.Item>
+          </Picker>
+        </Flex>
       </Flex>
-    </Flex>
+    </Fragment>
   );
 };
 
 const launchDate = "2020-02-12";
 const Eye = () => {
   return (
-    <Flex flex="auto" justifyContent="center">
+    <Flex flex="auto">
       <PreviewOrProjectPage
         launchDate={launchDate}
         PreviewPage={EyePreview}
