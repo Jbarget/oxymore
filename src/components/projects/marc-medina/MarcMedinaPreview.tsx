c2e9bb63824afa7c4e94deff34b3e7d9f8bcec12
sssoja <sophie@sssoja.com>
2021-02-15 17:00:03 +0000

    adds image assets and imports into content

---
+++ src/components/projects/marc-medina/MarcMedinaPreview.tsx
@@ -0,0 +1,24 @@
+import Flex from "../../Flex";
+import Icon from "../Icon";
+import React from "react";
+import Timer from "../../Timer";
+import mask from "../../../assets/project-page/project-icons/mask.png";
+
+const MarcMedinaPreview: React.FC<{ launchDate: string }> = ({
+  launchDate,
+}) => {
+  return (
+    <Flex
+      flex="auto"
+      overflow="hidden"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Icon icon={mask}></Icon>
+      <Timer launchDate={launchDate} />
+    </Flex>
+  );
+};
+
+export default MarcMedinaPreview;
