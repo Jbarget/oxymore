b45a6b438b2b3e7964b69dbf53fd98fbfa010137
sssoja <sophie@sssoja.com>
2021-02-22 18:23:29 +0000

    adds return to project page

--- src/components/projects/map/Map.tsx
+++ src/components/projects/map/Map.tsx
@@ -1,28 +1,53 @@
 import "react-inner-image-zoom/lib/InnerImageZoom/styles.min.css";
 
+import { LayoutProps, PositionProps, layout, position } from "styled-system";
+import React, { Fragment } from "react";
+
 import Flex from "../../Flex";
 // @ts-ignore
 import InnerImageZoom from "react-inner-image-zoom";
+import { Link } from "react-router-dom";
 import MapPreview from "./MapPreview";
+import { PROJECTS_URL } from "../../../constants/router-urls";
 import PreviewOrProjectPage from "../PreviewOrProjectPage";
-import React from "react";
+import magnify from "../../../assets/project-page/project-icons/magnify.png";
 import mapSmall from "../../../assets/project-page/map/mapa-barcelona-small.jpg";
+import styled from "styled-components";
+import theme from "../../theme";
+
+const ReturnToProjectsPage = styled(Link)<LayoutProps & PositionProps>`
+  ${layout};
+  ${position};
+  z-index: ${theme.zIndexes.inFront};
+  transition: all 1s ease;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
 
 const MapContent = () => {
   return (
-    <Flex>
+    <Fragment>
+      <ReturnToProjectsPage
+        to={PROJECTS_URL}
+        width={80}
+        position="absolute"
+        left={[20, 20, 40, 40]}
+        top={80}
+      >
+        <img src={magnify} alt="magnify icon" />
+      </ReturnToProjectsPage>
       <InnerImageZoom
         src={mapSmall}
         alt="David Macho's map of Barcelona"
         zoomScale={0.4}
         fullscreenOnMobile
-        moveType="drag"
       />
-    </Flex>
+    </Fragment>
   );
 };
 
-const launchDate = "2020-02-26";
+const launchDate = "2021-02-26";
 const Map = () => {
   return (
     <Flex
